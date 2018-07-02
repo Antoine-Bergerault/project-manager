8292a380b02987c8a9933f672a1ae1fb0dacd13d
Antoine Bergerault <antoine.bergerault@gmail.com>
2018-07-02 11:04:47 +0200

    update

--- tools/Message.js
+++ tools/Message.js
@@ -5,7 +5,7 @@ class Message {
         this.content = message.content;
         this.activator = '';
         this.tag = '';
-        this.serverID = this.discord.author.id;
+        this.serverID = 'users/'+this.discord.author.id;
         try{
             this.serverID = this.discord.guild.id;
         }catch(err){
@@ -60,11 +60,11 @@ class Message {
         if(txt != null){
             if(this.private()){
                 let msg = txt;
-                if(this.serverID != this.discord.author.id){
+                if(this.serverID != 'users/'+this.discord.author.id){
                     msg = "__Responding to :__ `"+this.discord.content+"`\n\n"+txt;
                 }
                 this.discord.author.send(msg);
-                if(this.serverID != this.discord.author.id){
+                if(this.serverID != 'users/'+this.discord.author.id){
                     this.discord.channel.send('The message has been sent to you');
                 }
             }else{
